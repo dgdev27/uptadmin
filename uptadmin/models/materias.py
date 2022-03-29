0383dd4f6ca459e0ab3868c851b267c3b79084e2
dgdev27 <odigregorio@oasisconsultora.com>
2022-03-29 18:15:28 -0400

    Actualización de rama windows

---
+++ uptadmin/models/materias.py
@@ -0,0 +1,21 @@
+from odoo import api, fields, models
+from odoo.osv import expression
+from datetime import datetime, timedelta
+
+class UPTAMaterias(models.Model):
+    _name = 'upta.materias'
+    _description = 'Materias de la UPT Aragua'
+
+    name = fields.Char(string='Nombre', help='Inserte el nombre de la materia a registrar', size=20)
+    codigo = fields.Char(string='Código', help='Inserte el código de la materia a registrar', size=10)
+    mallas_id = fields.Many2one(comodel_name='upta.mallas', string='Malla', help='Malla a la que está asignada la materia')
+    trayecto_id = fields.Many2one(comodel_name='upta.trayectos', string='Trayecto', help='Trayecto al que está asignada la materia')
+    seccion_id = fields.Many2many(comodel_name='upta.secciones', string='Secciones', help='Sección a la que está asignada la materia')
+    fases_id = fields.Many2one(comodel_name='upta.fases', string='Fase', help='Fase a la que está asignada la materia')
+    status = fields.Selection(string='Estado', selection=[('activo', 'Activo'), ('inactivo', 'Inactivo')], default="activo")
+
+    def status_activate(self):
+        self.status = 'activo'
+
+    def status_desactivate(self):
+        self.status = 'inactivo'
